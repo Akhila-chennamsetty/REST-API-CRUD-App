eec92f2c619ddd656b293eb23bc3906c4228ae8c
Akhila-chennamsetty <akhilachennamsetty9@gmail.com>
2025-05-27 11:15:18 +0530

    added sop in main

--- src/main/java/com/crud/RestApiCrudAppApplication.java
+++ src/main/java/com/crud/RestApiCrudAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApiCrudAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestApiCrudAppApplication.class, args);
+		System.out.println("REST API...");
 	}
 
 }
